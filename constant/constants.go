751ea9d31f8f69c26f6b64aebe3723824367dd41
vin-albertus <vinalbertus@gmail.com>
2023-03-12 16:48:38 +0700

    add constant file and tidy up code

---
+++ constant/constants.go
@@ -0,0 +1,14 @@
+package constant
+
+const (
+	MINDSET          = "mindset"
+	MOTIVATION       = "motivation"
+	HABITS           = "habits"
+	PRODUCTIVITY     = "productivity"
+	SELF_REMINDER    = "self reminder"
+	SELF_IMPROVEMENT = "self improvement"
+
+	HIGH_REACHMENT = "high"
+	MED_REACHMENT  = "medium"
+	LOW_REACHMENT  = "low"
+)
--- main.go
+++ main.go
@@ -5,42 +5,49 @@ import (
 	"fmt"
 	"math/rand"
 	"strings"
+
+	"github.com/AlbertusKevin/hashtag-grouper/constant"
 )
 
+func createMapHashtagGroup() map[string]map[string]string{
+	hashtagGroupPerNiche := map[string]map[string]string{}
+
+	hashtagGroupPerNiche[constant.MINDSET] = map[string]string{}
+	hashtagGroupPerNiche[constant.MINDSET][constant.HIGH_REACHMENT] = `#positivemindset #determination #thinkbig #mindsetmatters #selfconfidence #selfdevelopment #growthmindset #mindsetshift #mindsetofgreatness #changeyourmindset #successfulmindset #healthymindset`
+	hashtagGroupPerNiche[constant.MINDSET][constant.MED_REACHMENT] = `#mindsetiskey #mindsetreset #mindsetmastery #mindsetchange #mindsetmotivation #changeyourmindsetchangeyourlife #strongmindset #motivationalmindset #growthmindset🌱 #mindsetgrowth #mindsetgoals #mindsetforgreatness #postivemindset #shiftyourmindset #gratefulmindset #manifestationmindset #successisamindset #positivemindset💯`
+	hashtagGroupPerNiche[constant.MINDSET][constant.LOW_REACHMENT] = `#mindsetpositif #mindsetshifting #changemindset #itsamindset #mindsetovereverything #powerfulmindset #mindsetmatter #fixmindset #goodmindset #growthmindsets #successfulmindsets #happymindset #mindsetfirst #clearmindset #sucessfulmindset #mindsetpower #mindsetmaster #mindsetpositive #healthymindsets #growmindset #mindset101 #changingmindsets #evolveyourmindset #motivasimindset #powerofmindset`
+
+	hashtagGroupPerNiche[constant.MOTIVATION] = map[string]string{}
+	hashtagGroupPerNiche[constant.MOTIVATION][constant.HIGH_REACHMENT] = `#motivationforlife #motivationdaily #motivations #motivateyourself #motivationalwords #motivationoftheday #motivationalpost #motivation101 #inspiringquotes #selfmotivation #instamotivation`
+	hashtagGroupPerNiche[constant.MOTIVATION][constant.MED_REACHMENT] = `#lifemotivation #motivationlife #motivates #motivationeveryday #fearlessmotivation #getmotivated #strongmindset #motivationalmindset #successmind #inspirationalthoughts #motivationmindset #motivateothers #achieveyourgoals #lessonsoflife #actiontaker #motivation100 #beleiveinyourself #motivation💪 #lifechangingquotes #bestmotivation`
+	hashtagGroupPerNiche[constant.MOTIVATION][constant.LOW_REACHMENT] = `#motivationalquotesaboutlife #motivationquotesforlife #lifestylemotivation #motivationoflife #lifetimemotivation #lifechangingmotivation #motivationinlife #motivation4life #motivationalforlife #motivationallineaboutlife #lifesgoalsmotivation #lifegoalsmotivation #motivationlifestyle #motivatedminds #motivatedlife #motivationmilestone #motivationwords #successthoughts #mindthinking #thinkforchange #thinkforchanges #motivation_thoughts #power_of_positivity #motivation_of_the_day #motivation_line #instagrammotivation #inspirationmindset`
+
+	hashtagGroupPerNiche[constant.HABITS] = map[string]string{}
+	hashtagGroupPerNiche[constant.HABITS][constant.HIGH_REACHMENT] = `#betterlife #discipline #starttoday #consistency #routine #decisions #bebetter #willpower #choices #habits #healthyhabits #youcandoit #habit`
+	hashtagGroupPerNiche[constant.HABITS][constant.MED_REACHMENT] = `#habitandhome #habitacion #successhabits #goodhabits #dailyhabits #habitchange #highperformancehabits #changeyourhabits #starting #becomebetter #forwardthinking #selfdiscipline #workonyourself #longtermgoals #betteryourself #helpyourself #changelife`
+	hashtagGroupPerNiche[constant.HABITS][constant.LOW_REACHMENT] = `#changinghabits #successfulhabits #habitstacking #oldhabits #createhealthyhabits #betterhabits #habitsforhappiness #creatinghealthyhabits #habitsforlife #goodhabitsforlife #breakinghabits #buildinghabits #healtyhabits #createnewhabits #creatinghabits #breakbadhabits #habitsofmind #creategoodhabits #smallhabits #newhabitsnewresults #habitsandlifestyles #delinquenthabits #successfulpeoplehabits #buildhabits #habitsforwellness #mindfulhabits #goodhabitsstartyoung #selfcarehabits #greathabits #habitsofhappiness`
+
+	hashtagGroupPerNiche[constant.PRODUCTIVITY] = map[string]string{}
+	hashtagGroupPerNiche[constant.PRODUCTIVITY][constant.HIGH_REACHMENT] = `#productivity #timemanagement #productive #careergoals #planning #worklifebalance #organized #workmode #studytime`
+	hashtagGroupPerNiche[constant.PRODUCTIVITY][constant.MED_REACHMENT] = `#timemanagementtips #mindsetreset #productivityhacks #productivityhabits #goalplanning #gettingthingsdone #getthingsdone #decisionmaking #achievemore #planforsuccess #juststart #workgoals #makeaplan #scheduling #procrastinate #selfmanagement #organizedlife #selfknowledge #productivitytips #productiveday #beproductive #growthhacking #onmydesk #worksmart`
+	hashtagGroupPerNiche[constant.PRODUCTIVITY][constant.LOW_REACHMENT] = `#timemanagementskills #timemanagementhacks #timemanagement101 #timemanagementgoals #productivityplanner #productivitytip #productivityhack #lifeplanning #dailyhabitsforsuccess #taskmanagement #productivemindset #takeactiontoday #actionplan #worklifeintegration #developyourself #planyourday #stopprocrastinating #managementskills #timeblocking #lifeplan #getstuffdone #timemanagment #managementwaktu`
+
+	hashtagGroupPerNiche[constant.SELF_REMINDER] = map[string]string{}
+	hashtagGroupPerNiche[constant.SELF_REMINDER][constant.HIGH_REACHMENT] = `#selfreminder #selfreflection #reminder #notetoself #journaling #dailyreminder #selfawareness #selfhealing #selfworth #janganlupabahagia`
+	hashtagGroupPerNiche[constant.SELF_REMINDER][constant.MED_REACHMENT] = `#remindertoself #selfreminders #selfreflect #jadilebihbaik #selftalk #katasemangat #introspeksidiri #simplereminders #intropeksidiri #notetomyself #selfnote #pengingatdiri #dailyreminders #lebihbaik #perbaikidiri #reminders #journalingcommunity #nasehatdiri #journalwithme #salingmengingatkan #selfcareisntselfish #lifeadvice #affirmationsoftheday #anxietyfighter`
+	hashtagGroupPerNiche[constant.SELF_REMINDER][constant.LOW_REACHMENT] = `#reminderformyself #remindertomyself #reminderself #remindermyself #reminder4myself #selfcarereminder #selflovereminder #reminderstoself #reminderforself #reminderstomyself #selfireminder #reminder2self #selfreminder❤ #selfreminder😊 #thoughtfortoday #bepatientwithyourself #behopeful #renungandiri #reminderoftheday #mindfulllife #semangatberjuang #emotionalgrowth #soulpeace #ketenangan #jangantakut #selalusemangat #bodoamatlah`
+
+	hashtagGroupPerNiche[constant.SELF_IMPROVEMENT] = map[string]string{}
+	hashtagGroupPerNiche[constant.SELF_IMPROVEMENT][constant.HIGH_REACHMENT] = `#selfconfidence #selfempowerment #personalgrowth #selfgrowth #selfworth #personaldevelopment #selfimprovement #selfhelp #selfdiscovery #changeyourlife #innerstrength #selfrespect #selfesteem #bethebestyou #beyourbestself #youmatter`
+	hashtagGroupPerNiche[constant.SELF_IMPROVEMENT][constant.MED_REACHMENT] = `#selfimprovementdaily #selfmastery #becomebetter #strongmind #mindpower #positiveselftalk #findyourpurpose #healthyboundaries #personaldevelopmentjourney #selfdevelopmentjourney #selfdiscipline #selfeducation #selfmotivated #improveyourself #betteryourself #personalgrowthanddevelopment #selfgrowthjourney #personaljourney #smallsteps #yourjourney #lifejourney #selfhelptips #overcomingobstacles #selfaffirmation #valueyourself #selfvalue #overcomefear #selfactualization #thejourney #pengembangandiri`
+	hashtagGroupPerNiche[constant.SELF_IMPROVEMENT][constant.LOW_REACHMENT] = `#improvementofself #selfimprovements #selfimprovementquotes #selfimprovementtips #selfimprovementjourney #selfimprovementeveryday #selfimprovementsdaily #selfimprovementmovement #selfimprovementgoals #selfdevelopement #controlyourmind #lifemastery #selfimprove #selﬁmprovement #berubahlebihbaik #confidencefromwithin #lifeimprovement`
+
+	return hashtagGroupPerNiche
+}
+
 func main() {
-	var hashtagGroupPerNiche = map[string]map[string]string{}
-
-	hashtagGroupPerNiche["mindset"] = map[string]string{}
-	hashtagGroupPerNiche["mindset"]["high"] = `#positivemindset #determination #thinkbig #mindsetmatters #selfconfidence #selfdevelopment #growthmindset #mindsetshift #mindsetofgreatness #changeyourmindset #successfulmindset #healthymindset`
-	hashtagGroupPerNiche["mindset"]["medium"] = `#mindsetiskey #mindsetreset #mindsetmastery #mindsetchange #mindsetmotivation #changeyourmindsetchangeyourlife #strongmindset #motivationalmindset #growthmindset🌱 #mindsetgrowth #mindsetgoals #mindsetforgreatness #postivemindset #shiftyourmindset #gratefulmindset #manifestationmindset #successisamindset #positivemindset💯`
-	hashtagGroupPerNiche["mindset"]["low"] = `#mindsetpositif #mindsetshifting #changemindset #itsamindset #mindsetovereverything #powerfulmindset #mindsetmatter #fixmindset #goodmindset #growthmindsets #successfulmindsets #happymindset #mindsetfirst #clearmindset #sucessfulmindset #mindsetpower #mindsetmaster #mindsetpositive #healthymindsets #growmindset #mindset101 #changingmindsets #evolveyourmindset #motivasimindset #powerofmindset`
-
-	hashtagGroupPerNiche["motivation"] = map[string]string{}
-	hashtagGroupPerNiche["motivation"]["high"] = `#motivationforlife #motivationdaily #motivations #motivateyourself #motivationalwords #motivationoftheday #motivationalpost #motivation101 #inspiringquotes #selfmotivation #instamotivation`
-	hashtagGroupPerNiche["motivation"]["medium"] = `#lifemotivation #motivationlife #motivates #motivationeveryday #fearlessmotivation #getmotivated #strongmindset #motivationalmindset #successmind #inspirationalthoughts #motivationmindset #motivateothers #achieveyourgoals #lessonsoflife #actiontaker #motivation100 #beleiveinyourself #motivation💪 #lifechangingquotes #bestmotivation`
-	hashtagGroupPerNiche["motivation"]["low"] = `#motivationalquotesaboutlife #motivationquotesforlife #lifestylemotivation #motivationoflife #lifetimemotivation #lifechangingmotivation #motivationinlife #motivation4life #motivationalforlife #motivationallineaboutlife #lifesgoalsmotivation #lifegoalsmotivation #motivationlifestyle #motivatedminds #motivatedlife #motivationmilestone #motivationwords #successthoughts #mindthinking #thinkforchange #thinkforchanges #motivation_thoughts #power_of_positivity #motivation_of_the_day #motivation_line #instagrammotivation #inspirationmindset`
-
-	hashtagGroupPerNiche["habits"] = map[string]string{}
-	hashtagGroupPerNiche["habits"]["high"] = `#betterlife #discipline #starttoday #consistency #routine #decisions #bebetter #willpower #choices #habits #healthyhabits #youcandoit #habit`
-	hashtagGroupPerNiche["habits"]["medium"] = `#habitandhome #habitacion #successhabits #goodhabits #dailyhabits #habitchange #highperformancehabits #changeyourhabits #starting #becomebetter #forwardthinking #selfdiscipline #workonyourself #longtermgoals #betteryourself #helpyourself #changelife`
-	hashtagGroupPerNiche["habits"]["low"] = `#changinghabits #successfulhabits #habitstacking #oldhabits #createhealthyhabits #betterhabits #habitsforhappiness #creatinghealthyhabits #habitsforlife #goodhabitsforlife #breakinghabits #buildinghabits #healtyhabits #createnewhabits #creatinghabits #breakbadhabits #habitsofmind #creategoodhabits #smallhabits #newhabitsnewresults #habitsandlifestyles #delinquenthabits #successfulpeoplehabits #buildhabits #habitsforwellness #mindfulhabits #goodhabitsstartyoung #selfcarehabits #greathabits #habitsofhappiness`
-
-	hashtagGroupPerNiche["productivity"] = map[string]string{}
-	hashtagGroupPerNiche["productivity"]["high"] = `#productivity #timemanagement #productive #careergoals #planning #worklifebalance #organized #workmode #studytime`
-	hashtagGroupPerNiche["productivity"]["medium"] = `#timemanagementtips #mindsetreset #productivityhacks #productivityhabits #goalplanning #gettingthingsdone #getthingsdone #decisionmaking #achievemore #planforsuccess #juststart #workgoals #makeaplan #scheduling #procrastinate #selfmanagement #organizedlife #selfknowledge #productivitytips #productiveday #beproductive #growthhacking #onmydesk #worksmart`
-	hashtagGroupPerNiche["productivity"]["low"] = `#timemanagementskills #timemanagementhacks #timemanagement101 #timemanagementgoals #productivityplanner #productivitytip #productivityhack #lifeplanning #dailyhabitsforsuccess #taskmanagement #productivemindset #takeactiontoday #actionplan #worklifeintegration #developyourself #planyourday #stopprocrastinating #managementskills #timeblocking #lifeplan #getstuffdone #timemanagment #managementwaktu`
-
-	hashtagGroupPerNiche["self reminder"] = map[string]string{}
-	hashtagGroupPerNiche["self reminder"]["high"] = `#selfreminder #selfreflection #reminder #notetoself #journaling #dailyreminder #selfawareness #selfhealing #selfworth #janganlupabahagia`
-	hashtagGroupPerNiche["self reminder"]["medium"] = `#remindertoself #selfreminders #selfreflect #jadilebihbaik #selftalk #katasemangat #introspeksidiri #simplereminders #intropeksidiri #notetomyself #selfnote #pengingatdiri #dailyreminders #lebihbaik #perbaikidiri #reminders #journalingcommunity #nasehatdiri #journalwithme #salingmengingatkan #selfcareisntselfish #lifeadvice #affirmationsoftheday #anxietyfighter`
-	hashtagGroupPerNiche["self reminder"]["low"] = `#reminderformyself #remindertomyself #reminderself #remindermyself #reminder4myself #selfcarereminder #selflovereminder #reminderstoself #reminderforself #reminderstomyself #selfireminder #reminder2self #selfreminder❤ #selfreminder😊 #thoughtfortoday #bepatientwithyourself #behopeful #renungandiri #reminderoftheday #mindfulllife #semangatberjuang #emotionalgrowth #soulpeace #ketenangan #jangantakut #selalusemangat #bodoamatlah`
-
-	hashtagGroupPerNiche["self improvement"] = map[string]string{}
-	hashtagGroupPerNiche["self improvement"]["high"] = `#selfconfidence #selfempowerment #personalgrowth #selfgrowth #selfworth #personaldevelopment #selfimprovement #selfhelp #selfdiscovery #changeyourlife #innerstrength #selfrespect #selfesteem #bethebestyou #beyourbestself #youmatter`
-	hashtagGroupPerNiche["self improvement"]["medium"] = `#selfimprovementdaily #selfmastery #becomebetter #strongmind #mindpower #positiveselftalk #findyourpurpose #healthyboundaries #personaldevelopmentjourney #selfdevelopmentjourney #selfdiscipline #selfeducation #selfmotivated #improveyourself #betteryourself #personalgrowthanddevelopment #selfgrowthjourney #personaljourney #smallsteps #yourjourney #lifejourney #selfhelptips #overcomingobstacles #selfaffirmation #valueyourself #selfvalue #overcomefear #selfactualization #thejourney #pengembangandiri`
-	hashtagGroupPerNiche["self improvement"]["low"] = `#improvementofself #selfimprovements #selfimprovementquotes #selfimprovementtips #selfimprovementjourney #selfimprovementeveryday #selfimprovementsdaily #selfimprovementmovement #selfimprovementgoals #selfdevelopement #controlyourmind #lifemastery #selfimprove #selﬁmprovement #berubahlebihbaik #confidencefromwithin #lifeimprovement`
-
-	makeHashtagNicheGroup(5, 20, hashtagGroupPerNiche["self reminder"])
+	hashtagGroupPerNiche := createMapHashtagGroup()
+	makeHashtagNicheGroup(5, 14, hashtagGroupPerNiche[constant.MOTIVATION])
 }
 
 func makeHashtagNicheGroup(nGroupResult int, nHashtag int, hashtagGroupPerNiche map[string]string){
@@ -52,7 +59,6 @@ func makeHashtagNicheGroup(nGroupResult int, nHashtag int, hashtagGroupPerNiche 
 	mediumGroup := map[int][]string{}
 	lowGroup := map[int][]string{}
 
-
 	// rasio komposisi hashtag dari setiap grup
 	var highRatio float64 = 1
 	var mediumRatio float64 = 3
@@ -61,14 +67,14 @@ func makeHashtagNicheGroup(nGroupResult int, nHashtag int, hashtagGroupPerNiche 
 	var total float64 = highRatio + mediumRatio + lowRatio
 
 	// hitung jumlah hashtag dalam grup sesuai ratio dari masing-masing tingkat reachment
-	countHighHashtagGroup := (highRatio/total)  * float64(nHashtag)
+	countHighHashtagGroup := (highRatio/total) * float64(nHashtag)
 	countMediumHashtagGroup := (mediumRatio/total) * float64(nHashtag)
 	countLowHashtagGroup := (lowRatio/total) * float64(nHashtag)
 
 	// jadikan string menjadi slice of string
-	highReachmentHashtags := strings.Split(hashtagGroupPerNiche["high"]," ")
-	mediumReachmentHashtags := strings.Split(hashtagGroupPerNiche["medium"]," ")
-	lowReachmentHashtags := strings.Split(hashtagGroupPerNiche["low"]," ")
+	highReachmentHashtags := strings.Split(hashtagGroupPerNiche[constant.HIGH_REACHMENT]," ")
+	mediumReachmentHashtags := strings.Split(hashtagGroupPerNiche[constant.MED_REACHMENT]," ")
+	lowReachmentHashtags := strings.Split(hashtagGroupPerNiche[constant.LOW_REACHMENT]," ")
 
 	// buat n grup per masing-masing level
 	for i := 0; i < nGroupResult; i++{
